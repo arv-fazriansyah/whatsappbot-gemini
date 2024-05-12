3f0f1370c72856e416da2a3ba09f9fc74c76db2e
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2024-05-13 04:52:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,185 +1,163 @@
-const {
-    default: makeWASocket,
-    DisconnectReason,
-    fetchLatestBaileysVersion,
-    isJidBroadcast,
-    makeInMemoryStore,
-    useMultiFileAuthState} = require("@whiskeysockets/baileys");
-
-const apiURL = "https://api.arv-serverless.workers.dev/v1/chat/completions"
-const log = (pino = require("pino"));
-const { session } = { "session": "baileys_auth_info" };
+const { default: makeWASocket, DisconnectReason, fetchLatestBaileysVersion, isJidBroadcast, makeInMemoryStore, useMultiFileAuthState } = require("@whiskeysockets/baileys");
 const { Boom } = require("@hapi/boom");
-const path = require('path');
 const express = require("express");
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const app = require("express")()
-// enable files upload
-app.use(fileUpload({
-    createParentPath: true
-}));
+const http = require("http");
+const path = require('path');
+const qrcode = require("qrcode");
+const pino = require("pino");
+
+const apiURL = "https://api.arv-serverless.workers.dev/v1/chat/completions";
+const session = "baileys_auth_info";
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-const port = process.env.PORT || 8000;
-const qrcode = require("qrcode");
-
+app.use(fileUpload({ createParentPath: true }));
 app.use("/assets", express.static(path.join(__dirname, "client", "assets")));
 
-app.get("/scan", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "server.html"));
-});
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "index.html"));
-});
+app.get("/scan", (req, res) => res.sendFile(path.join(__dirname, "client", "server.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "client", "index.html")));
 
 const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }) });
-
 let sock;
 let qr;
 let soket;
 
 async function connectToWhatsApp() {
-    const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info')
-    let { version } = await fetchLatestBaileysVersion();
+    const { state, saveCreds } = await useMultiFileAuthState(session);
+    const { version } = await fetchLatestBaileysVersion();
     sock = makeWASocket({
         printQRInTerminal: true,
         auth: state,
-        logger: log({ level: "silent" }),
+        logger: pino({ level: "silent" }),
         version,
-        shouldIgnoreJid: jid => isJidBroadcast(jid),
+        shouldIgnoreJid: isJidBroadcast,
     });
     store.bind(sock.ev);
-    sock.multi = true
-    sock.ev.on('connection.update', async (update) => {
-        const { connection, lastDisconnect } = update;
-        if (connection === 'close') {
-            let reason = new Boom(lastDisconnect.error).output.statusCode;
-            switch (reason) {
-                case DisconnectReason.badSession:
-                    console.log(`Bad Session File, Please Delete ${session} and Scan Again`);
-                    sock.logout();
-                    break;
-                case DisconnectReason.connectionClosed:
-                    console.log("Connection closed, reconnecting....");
-                    connectToWhatsApp();
-                    break;
-                case DisconnectReason.connectionLost:
-                    console.log("Connection Lost from Server, reconnecting...");
-                    connectToWhatsApp();
-                    break;
-                case DisconnectReason.connectionReplaced:
-                    console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First");
-                    sock.logout();
-                    break;
-                case DisconnectReason.loggedOut:
-                    console.log(`Device Logged Out, Please Delete ${session} and Scan Again.`);
-                    sock.logout();
-                    break;
-                case DisconnectReason.restartRequired:
-                    console.log("Restart Required, Restarting...");
-                    connectToWhatsApp();
-                    break;
-                case DisconnectReason.timedOut:
-                    console.log("Connection TimedOut, Reconnecting...");
-                    connectToWhatsApp();
-                    break;
-                default:
-                    sock.end(`Unknown DisconnectReason: ${reason}|${lastDisconnect.error}`);
-                    break;
-            }
-        } else if (connection === 'open') {
-            console.log('opened connection');
-            return;
-        }
-        if (update.qr) {
-            qr = update.qr;
-            updateQR("qr");
-        }
-        else if (qr = undefined) {
-            updateQR("loading");
-        }
-        else {
-            if (update.connection === "open") {
-                updateQR("qrscanned");
-                return;
-            }
-        }
-    });
+    sock.multi = true;
+    sock.ev.on('connection.update', handleConnectionUpdate);
     sock.ev.on("creds.update", saveCreds);
-    sock.ev.on("messages.upsert", async ({ messages, type }) => {
-        if (type === "notify" && !messages[0].key.fromMe) {
-            const pesan = messages[0].message.conversation;
-            const noWa = messages[0].key.remoteJid;
-            await sock.readMessages([messages[0].key]);
-            try {
-                const response = await fetch(apiURL, {
-                    method: 'POST',
-                    body: JSON.stringify({ messages: [{ role: "user", content: pesan }] }),
-                    headers: { 'Content-Type': 'application/json' },
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    const gptMessage = data.choices[0].message.content;
-                    await sock.sendMessage(noWa, { text: gptMessage }, { quoted: messages[0] });
-                } else {
-                    console.error('Failed to fetch from ChatGPT:', response.statusText);
-                    // Send error message to WhatsApp
-                    await sock.sendMessage(noWa, { text: `Error: ${response.statusText}` }, { quoted: messages[0] });
-                }
-            } catch (error) {
-                console.error('Error:', error);
-                // Send error message to WhatsApp
-                await sock.sendMessage(noWa, { text: `Error: ${error.message}` }, { quoted: messages[0] });
+    sock.ev.on("messages.upsert", handleMessagesUpsert);
+}
+
+function handleConnectionUpdate(update) {
+    const { connection, lastDisconnect } = update;
+    switch (connection) {
+        case 'close':
+            handleConnectionClose(lastDisconnect.error);
+            break;
+        case 'open':
+            console.log('opened connection');
+            break;
+        default:
+            break;
+    }
+    if (update.qr) {
+        qr = update.qr;
+        updateQR("qr");
+    } else if (!qr) {
+        updateQR("loading");
+    } else if (update.connection === "open") {
+        updateQR("qrscanned");
+    }
+}
+
+async function handleMessagesUpsert({ messages, type }) {
+    if (type === "notify" && !messages[0].key.fromMe) {
+        const pesan = messages[0].message.conversation;
+        const noWa = messages[0].key.remoteJid;
+        await sock.readMessages([messages[0].key]);
+        try {
+            const response = await fetch(apiURL, {
+                method: 'POST',
+                body: JSON.stringify({ messages: [{ role: "user", content: pesan }] }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (response.ok) {
+                const data = await response.json();
+                const gptMessage = data.choices[0].message.content;
+                await sock.sendMessage(noWa, { text: gptMessage }, { quoted: messages[0] });
+            } else {
+                console.error('Failed to fetch from ChatGPT:', response.statusText);
+                await sock.sendMessage(noWa, { text: `Error: ${response.statusText}` }, { quoted: messages[0] });
             }
+        } catch (error) {
+            console.error('Error:', error);
+            await sock.sendMessage(noWa, { text: `Error: ${error.message}` }, { quoted: messages[0] });
         }
-    });
+    }
+}
+
+function handleConnectionClose(error) {
+    const reason = new Boom(error).output.statusCode;
+    switch (reason) {
+        case DisconnectReason.badSession:
+            console.log(`Bad Session File, Please Delete ${session} and Scan Again`);
+            sock.logout();
+            break;
+        case DisconnectReason.connectionClosed:
+        case DisconnectReason.connectionLost:
+        case DisconnectReason.restartRequired:
+        case DisconnectReason.timedOut:
+            console.log("Reconnecting...");
+            connectToWhatsApp();
+            break;
+        case DisconnectReason.connectionReplaced:
+        case DisconnectReason.loggedOut:
+            console.log(`Device Logged Out, Please Delete ${session} and Scan Again.`);
+            sock.logout();
+            break;
+        default:
+            sock.end(`Unknown DisconnectReason: ${reason}|${error}`);
+            break;
+    }
+}
+
+function isConnected() {
+    return !!sock.user;
+}
+
+function updateQR(data) {
+    const statusMap = {
+        "qr": () => {
+            qrcode.toDataURL(qr, (err, url) => {
+                soket?.emit("qr", url);
+                soket?.emit("log", "QR Code received, please scan!");
+            });
+        },
+        "connected": () => {
+            soket?.emit("qrstatus", "./assets/check.svg");
+            soket?.emit("log", "WhatsApp terhubung!");
+        },
+        "qrscanned": () => {
+            soket?.emit("qrstatus", "./assets/check.svg");
+            soket?.emit("log", "QR Code Telah discan!");
+        },
+        "loading": () => {
+            soket?.emit("qrstatus", "./assets/loader.gif");
+            soket?.emit("log", "Registering QR Code , please wait!");
+        },
+    };
+    const updateFunction = statusMap[data];
+    if (updateFunction) updateFunction();
 }
 
 io.on("connection", async (socket) => {
     soket = socket;
-    if (isConnected) {
+    if (isConnected()) {
         updateQR("connected");
     } else if (qr) {
         updateQR("qr");
     }
 });
-
-// functions
-const isConnected = () => {
-    return (sock.user);
-};
-
-const updateQR = (data) => {
-    switch (data) {
-        case "qr":
-            qrcode.toDataURL(qr, (err, url) => {
-                soket?.emit("qr", url);
-                soket?.emit("log", "QR Code received, please scan!");
-            });
-            break;
-        case "connected":
-            soket?.emit("qrstatus", "./assets/check.svg");
-            soket?.emit("log", "WhatsApp terhubung!");
-            break;
-        case "qrscanned":
-            soket?.emit("qrstatus", "./assets/check.svg");
-            soket?.emit("log", "QR Code Telah discan!");
-            break;
-        case "loading":
-            soket?.emit("qrstatus", "./assets/loader.gif");
-            soket?.emit("log", "Registering QR Code , please wait!");
-            break;
-        default:
-            break;
-    }
-};
 
 connectToWhatsApp().catch(err => console.log("unexpected error: " + err));
 server.listen(port, () => {
