725725c96e01f6477c17359325a86e3799ee254a
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2024-05-17 19:45:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const {
     msgRetryCounterMap
 } = require("@whiskeysockets/baileys");
 
-const log = (pino = require("pino"));
+const pino = require("pino");
+const log = pino();
 const { session } = { "session": "baileys_auth_info" };
 const { Boom } = require("@hapi/boom");
 const path = require('path');
@@ -27,14 +28,16 @@ const express = require("express");
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const bodyParser = require("body-parser");
-const app = require("express")()
-const server = require("http").createServer(app);
+const app = express();
+const server = http.createServer(app);
 const io = require("socket.io")(server);
 const dotenv = require("dotenv");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const port = process.env.PORT || 8000;
 const qrcode = require("qrcode");
 
+dotenv.config();
+
+const port = process.env.PORT || 8000;
 const allowedGroupJIDs = process.env.GROUP_ID;
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: process.env.MODEL });
@@ -45,8 +48,6 @@ let sock;
 let qr;
 let soket;
 let chatHistory = {};
-
-dotenv.config();
 
 app.use(fileUpload({
     createParentPath: true
@@ -67,7 +68,7 @@ const generationConfig = {
 };
 
 async function connectToWhatsApp() {
-    const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info')
+    const { state, saveCreds } = await useMultiFileAuthState('baileys_auth_info');
     let { version, isLatest } = await fetchLatestBaileysVersion();
     sock = makeWASocket({
         printQRInTerminal: true,
@@ -76,101 +77,99 @@ async function connectToWhatsApp() {
         version,
         shouldIgnoreJid: jid => isJidBroadcast(jid),
     });
+
     store.bind(sock.ev);
-    sock.multi = true
+    sock.multi = true;
+
     sock.ev.on('connection.update', async (update) => {
-        //console.log(update);
         const { connection, lastDisconnect } = update;
         if (connection === 'close') {
             let reason = new Boom(lastDisconnect.error).output.statusCode;
-            if (reason === DisconnectReason.badSession) {
-                console.log(`Bad Session File, Please Delete ${session} and Scan Again`);
-                sock.logout();
-            } else if (reason === DisconnectReason.connectionClosed) {
-                console.log("Connection closed, reconnecting....");
-                connectToWhatsApp();
-            } else if (reason === DisconnectReason.connectionLost) {
-                console.log("Connection Lost from Server, reconnecting...");
-                connectToWhatsApp();
-            } else if (reason === DisconnectReason.connectionReplaced) {
-                console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First");
-                sock.logout();
-            } else if (reason === DisconnectReason.loggedOut) {
-                console.log(`Device Logged Out, Please Delete ${session} and Scan Again.`);
-                sock.logout();
-            } else if (reason === DisconnectReason.restartRequired) {
-                console.log("Restart Required, Restarting...");
-                connectToWhatsApp();
-            } else if (reason === DisconnectReason.timedOut) {
-                console.log("Connection TimedOut, Reconnecting...");
-                connectToWhatsApp();
-            } else {
-                sock.end(`Unknown DisconnectReason: ${reason}|${lastDisconnect.error}`);
+            switch (reason) {
+                case DisconnectReason.badSession:
+                    console.log(`Bad Session File, Please Delete ${session} and Scan Again`);
+                    sock.logout();
+                    break;
+                case DisconnectReason.connectionClosed:
+                case DisconnectReason.connectionLost:
+                    console.log("Connection closed/lost, reconnecting....");
+                    connectToWhatsApp();
+                    break;
+                case DisconnectReason.connectionReplaced:
+                    console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First");
+                    sock.logout();
+                    break;
+                case DisconnectReason.loggedOut:
+                    console.log(`Device Logged Out, Please Delete ${session} and Scan Again.`);
+                    sock.logout();
+                    break;
+                case DisconnectReason.restartRequired:
+                    console.log("Restart Required, Restarting...");
+                    connectToWhatsApp();
+                    break;
+                case DisconnectReason.timedOut:
+                    console.log("Connection TimedOut, Reconnecting...");
+                    connectToWhatsApp();
+                    break;
+                default:
+                    sock.end(`Unknown DisconnectReason: ${reason}|${lastDisconnect.error}`);
             }
         } else if (connection === 'open') {
-            console.log('opened connection');
-            let getGroups = await sock.groupFetchAllParticipating();
-            let groups = Object.values(await sock.groupFetchAllParticipating())
-            //console.log(groups);
-            for (let group of groups) {
-                console.log("id_group: " + group.id + " || Nama Group: " + group.subject);
-            }
-            return;
+            console.log('Opened connection');
+            let groups = Object.values(await sock.groupFetchAllParticipating());
+            groups.forEach(group => {
+                console.log(`id_group: ${group.id} || Nama Group: ${group.subject}`);
+            });
         }
+
         if (update.qr) {
             qr = update.qr;
             updateQR("qr");
-        }
-        else if (qr = undefined) {
+        } else if (qr === undefined) {
             updateQR("loading");
-        }
-        else {
-            if (update.connection === "open") {
-                updateQR("qrscanned");
-                return;
-            }
+        } else if (update.connection === "open") {
+            updateQR("qrscanned");
         }
     });
-    sock.ev.on("creds.update", saveCreds);
-    sock.ev.on("messages.upsert", async ({ messages, type }) => {
-        if (type === "notify") {
-            if (!messages[0].key.fromMe) {
-                const message = messages[0];
-                const sender = message.key.remoteJid;
 
-                if (!allowedGroupJIDs.includes(sender)) {
-                    return;
+    sock.ev.on("creds.update", saveCreds);
+
+    sock.ev.on("messages.upsert", async ({ messages, type }) => {
+        if (type === "notify" && !messages[0].key.fromMe) {
+            const message = messages[0];
+            const sender = message.key.remoteJid;
+
+            if (!allowedGroupJIDs.includes(sender)) return;
+
+            const messageContent = message.message.conversation || (message.message.extendedTextMessage && message.message.extendedTextMessage.text);
+            if (!messageContent) return;
+
+            const incomingMessage = messageContent.toLowerCase();
+            const formattedSender = `+${sender.match(/\d+/)[0]}`;
+
+            await sock.readMessages([message.key]);
+            await sock.sendPresenceUpdate("composing", sender);
+
+            if (incomingMessage === "/new") {
+                await sock.sendMessage(sender, { text: `Conversation ID: ${formattedSender}` }, { quoted: message });
+            } else {
+                if (!chatHistory[sender]) {
+                    chatHistory[sender] = [
+                        { role: "user", parts: [{ text: `Halo, nama saya: ${message.pushName}` }] },
+                        { role: "model", parts: [{ text: "Halo, aku Veronisa dirancang oleh fazriansyah.my.id. Asisten yang sangat membantu, kreatif, pintar, dan ramah." }] },
+                    ];
                 }
 
-                const messageContent = message.message.conversation || (message.message.extendedTextMessage && message.message.extendedTextMessage.text);
-                if (!messageContent) return;
-                const incomingMessage = messageContent.toLowerCase();
-                const formattedSender = `+${sender.match(/\d+/)[0]}`;
+                const chat = model.startChat({ generationConfig, history: chatHistory[sender] });
+                const result = await chat.sendMessage(incomingMessage);
+                const response = (await result.response).text().replace(/\*\*/g, '*');
 
-                await sock.readMessages([message.key]);
-                await sock.sendPresenceUpdate("composing", sender);
-
-                if (!messages[0].key.fromMe && incomingMessage === "/new") {
-                    await sock.sendMessage(sender, { text: `Conversation ID: ${formattedSender}` }, { quoted: message });
+                if (!response) {
+                    await sock.sendMessage(sender, { text: "Maaf, terjadi kesalahan. Silakan coba lagi." }, { quoted: message });
+                    delete chatHistory[sender];
                 } else {
-                    if (!chatHistory[sender]) {
-                        chatHistory[sender] = [
-                            { role: "user", parts: [{ text: `Halo, nama saya: ${message.pushName}` }] },
-                            { role: "model", parts: [{ text: "Halo, aku Veronisa dirancang oleh fazriansyah.my.id. Asisten yang sangat membantu, kreatif, pintar, dan ramah." }] },
-                        ];
-                    }
-
-                    const chat = model.startChat({ generationConfig, history: chatHistory[sender] });
-                    const result = await chat.sendMessage(incomingMessage);
-                    const response = (await result.response).text().replace(/\*\*/g, '*');
-
-                    if (!response) {
-                        await sock.sendMessage(sender, { text: "Maaf, terjadi kesalahan. Silakan coba lagi." }, { quoted: message });
-                        delete chatHistory[sender];
-                    } else {
-                        console.log(JSON.stringify(chatHistory[sender]));
-                        await sock.sendMessage(sender, { text: response }, { quoted: message });
-                    }
+                    console.log(JSON.stringify(chatHistory[sender]));
+                    await sock.sendMessage(sender, { text: response }, { quoted: message });
                 }
             }
         }
@@ -179,17 +178,14 @@ async function connectToWhatsApp() {
 
 io.on("connection", async (socket) => {
     soket = socket;
-    // console.log(sock)
-    if (isConnected) {
+    if (isConnected()) {
         updateQR("connected");
     } else if (qr) {
         updateQR("qr");
     }
 });
 
-const isConnected = () => {
-    return (sock.user);
-};
+const isConnected = () => sock && sock.user;
 
 const updateQR = (data) => {
     switch (data) {
@@ -209,15 +205,14 @@ const updateQR = (data) => {
             break;
         case "loading":
             soket?.emit("qrstatus", "./assets/loader.gif");
-            soket?.emit("log", "Registering QR Code , please wait!");
-            break;
-        default:
+            soket?.emit("log", "Registering QR Code, please wait!");
             break;
     }
 };
 
 connectToWhatsApp()
-    .catch(err => console.log("unexpected error: " + err)) 
+    .catch(err => console.log("unexpected error: " + err));
+
 server.listen(port, () => {
     console.log("Server running on port:", port);
 });
